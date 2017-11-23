4c9e539db06887700b785d148271f542adbb9b59
jomonkjoy <jomonkurianjoy@gmail.com>
2017-11-23 18:06:03 +0000

    Update fft_complex_compute.sv

--- fft_complex_compute.sv
+++ fft_complex_compute.sv
@@ -10,13 +10,13 @@ module fft_complex_compute #(
 );
   
   typedef struct packed {
-    logic [DATA_WIDTH/2-1:0] real;
-    logic [DATA_WIDTH/2-1:0] imag;
+    logic [DATA_WIDTH/2-1:0] r;
+    logic [DATA_WIDTH/2-1:0] i;
   } complex_type1;
   
   typedef struct packed {
-    logic [DATA_WIDTH-1:0] real;
-    logic [DATA_WIDTH-1:0] imag;
+    logic [DATA_WIDTH-1:0] r;
+    logic [DATA_WIDTH-1:0] i;
   } complex_type2;
   
   complex_type1 reg_x_N;
@@ -35,18 +35,18 @@ module fft_complex_compute #(
   end
   // pipeline multiply-add outputs
   always_ff @(posedge clk) begin
-    mul_y_N.real <= {{DATA_WIDTH{reg_x_N.real[DATA_WIDTH-1]}},reg_x_N.real} + reg_w_N.real*reg_x_M.real;
-    mul_y_M.real <= {{DATA_WIDTH{reg_x_N.real[DATA_WIDTH-1]}},reg_x_N.real} - reg_w_N.real*reg_x_M.real;
-    mul_y_N.imag <= {{DATA_WIDTH{reg_x_N.imag[DATA_WIDTH-1]}},reg_x_N.imag} + reg_w_N.imag*reg_x_M.imag;
-    mul_y_M.imag <= {{DATA_WIDTH{reg_x_N.imag[DATA_WIDTH-1]}},reg_x_N.imag} - reg_w_N.imag*reg_x_M.imag;
+    mul_y_N.r <= {{DATA_WIDTH{reg_x_N.r[DATA_WIDTH-1]}},reg_x_N.r} + reg_w_N.r*reg_x_M.r;
+    mul_y_M.r <= {{DATA_WIDTH{reg_x_N.r[DATA_WIDTH-1]}},reg_x_N.r} - reg_w_N.r*reg_x_M.r;
+    mul_y_N.i <= {{DATA_WIDTH{reg_x_N.i[DATA_WIDTH-1]}},reg_x_N.i} + reg_w_N.i*reg_x_M.i;
+    mul_y_M.i <= {{DATA_WIDTH{reg_x_N.i[DATA_WIDTH-1]}},reg_x_N.i} - reg_w_N.i*reg_x_M.i;
   end
   
-  assign xor_y_N.real = mul_y_N.real[2*DATA_WIDTH-1:DATA_WIDTH] ^ mul_y_N.real[DATA_WIDTH-1:0];
-  assign xor_y_M.real = mul_y_M.real[2*DATA_WIDTH-1:DATA_WIDTH] ^ mul_y_M.real[DATA_WIDTH-1:0];
-  assign xor_y_N.imag = mul_y_N.imag[2*DATA_WIDTH-1:DATA_WIDTH] ^ mul_y_N.imag[DATA_WIDTH-1:0];
-  assign xor_y_M.imag = mul_y_M.imag[2*DATA_WIDTH-1:DATA_WIDTH] ^ mul_y_M.imag[DATA_WIDTH-1:0];
+  assign xor_y_N.r = mul_y_N.r[2*DATA_WIDTH-1:DATA_WIDTH] ^ mul_y_N.r[DATA_WIDTH-1:0];
+  assign xor_y_M.r = mul_y_M.r[2*DATA_WIDTH-1:DATA_WIDTH] ^ mul_y_M.r[DATA_WIDTH-1:0];
+  assign xor_y_N.i = mul_y_N.i[2*DATA_WIDTH-1:DATA_WIDTH] ^ mul_y_N.i[DATA_WIDTH-1:0];
+  assign xor_y_M.i = mul_y_M.i[2*DATA_WIDTH-1:DATA_WIDTH] ^ mul_y_M.i[DATA_WIDTH-1:0];
   
-  assign y_N = {xor_y_N.real,xor_y_N.imag};
-  assign y_M = {xor_y_M.real,xor_y_M.imag};
+  assign y_N = {xor_y_N.r,xor_y_N.i};
+  assign y_M = {xor_y_M.r,xor_y_M.i};
   
 endmodule
